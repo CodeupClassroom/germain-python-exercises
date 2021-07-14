7ac8848d1b256cd0aa2af6e42b4c5a6dd9175719
Ryan Orsinger <ryan.orsinger@gmail.com>
2021-07-14 12:48:31 -0500

    add import exercise walkthrough

--- util.py
+++ util.py
@@ -1,3 +1,19 @@
+def get_letter_grade(number):
+    if number <= 69:
+        grade = "F"
+    elif number <= 79:
+        grade = "C"
+    elif number <= 89:
+        grade = "B"
+    else:
+        grade = "A"
+    return grade
+
+
+def calculate_tip(bill, tip_percent):    
+    return bill * tip_percent
+
+
 def is_vowel(x):
     # Anything not a string is definitely not a vowel
     if type(x) != str:
@@ -16,30 +32,32 @@ def is_consonant(x):
     return result
 
 
-letter = "A"
-print(f"Is the letter {letter} a vowel? {is_vowel(letter)}")
-print(f"Is the letter {letter} a consonant? {is_consonant(letter)}")
+# Procedural code indented here will only run if we explicitly run this file from the command line
+if __name__ == '__main__':
+    letter = "A"
+    print(f"Is the letter {letter} a vowel? {is_vowel(letter)}")
+    print(f"Is the letter {letter} a consonant? {is_consonant(letter)}")
 
-# print() prints an empty new line
-print() 
+    # print() prints an empty new line
+    print() 
 
-letter = "B"
-print(f"Is the letter {letter} a vowel? {is_vowel(letter)}")
-print(f"Is the letter {letter} a consonant? {is_consonant(letter)}")
+    letter = "B"
+    print(f"Is the letter {letter} a vowel? {is_vowel(letter)}")
+    print(f"Is the letter {letter} a consonant? {is_consonant(letter)}")
 
-print() 
+    print() 
 
-letter = "X"
-print(f"Is the letter {letter} a vowel? {is_vowel(letter)}")
-print(f"Is the letter {letter} a consonant? {is_consonant(letter)}")
+    letter = "X"
+    print(f"Is the letter {letter} a vowel? {is_vowel(letter)}")
+    print(f"Is the letter {letter} a consonant? {is_consonant(letter)}")
 
-print()
+    print()
 
-letter = "E"
-print(f"Is the letter {letter} a vowel? {is_vowel(letter)}")
-print(f"Is the letter {letter} a consonant? {is_consonant(letter)}")
+    letter = "E"
+    print(f"Is the letter {letter} a vowel? {is_vowel(letter)}")
+    print(f"Is the letter {letter} a consonant? {is_consonant(letter)}")
 
-print()
-letter = "$"
-print(f"Is the letter {letter} a vowel? {is_vowel(letter)}")
-print(f"Is the letter {letter} a consonant? {is_consonant(letter)}")
+    print()
+    letter = "$"
+    print(f"Is the letter {letter} a vowel? {is_vowel(letter)}")
+    print(f"Is the letter {letter} a consonant? {is_consonant(letter)}")
